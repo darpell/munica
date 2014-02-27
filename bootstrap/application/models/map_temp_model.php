dcf321b7393b501e0f9c894f155f263643afc9e5
darpell <pelonia.darius@gmail.com>
2014-02-27 23:14:06 +0800

    [TESTING] testing of map data in dashboard.

---
+++ bootstrap/application/models/map_temp_model.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Map_temp_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+		//load monica database
+		$this->load->database('default');
+	}
+	
+	function get_map_nodes($type = FALSE)
+	{
+		$this->db->from('map_nodes');
+		
+		if ($type != FALSE)
+				$this->db->where('node_type',$type);
+		
+		$query = $this->db->get();
+		return $query->result_array();
+			$query->free_result();
+	}
+}
+
+/* End of map_temp_model.php */
+/* Location: ./application/models/map_temp_model.php */
--- bootstrap/scripts/dashboard_s/map.js
+++ bootstrap/scripts/dashboard_s/map.js
@@ -1,81 +1,56 @@
-var center = new google.maps.LatLng(24.886436490787712, -70.2685546875);
+var dasma = new google.maps.LatLng(14.2990183, 120.9589699);
 function initialize()
 {
-	var mapProp = {
-					  center: center,
-					  zoom: 5,
-					  mapTypeId:google.maps.MapTypeId.TERRAIN
-				  };
-	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-	var outerCoords = [
-	                      new google.maps.LatLng(25.774252, -80.190262),
-	                      new google.maps.LatLng(18.466465, -66.118292),
-	                      new google.maps.LatLng(32.321384, -64.75737)
-	                  ];
-	// Construct the polygon
-    var bermudaTriangle = new google.maps.Polygon({
-      paths: outerCoords,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35
-    });
-    //alert(new google.maps.MVCArray.getArray(bermudaTriangle.getPaths()));
-    //var test = bermudaTriangle.getPaths();
-    //alert(test);
-    bermudaTriangle.setMap(map);
-
-    
-    generateMarkers(bermudaTriangle,map,5,outerCoords);
-}
-
-function generateMarkers(polygon,map,ctr,boundaries)
-{
-	var pointInt = 0;
-	var mark = new Array();
-	var marker = new Array();
-	var infowindow = new Array();
-	var infoListener = new Array();
-	while (pointInt < ctr)
-	{
-		mark[pointInt] = new google.maps.LatLng(getRandomInRange(boundaries[1].lat(),boundaries[2].lat(),6), getRandomInRange(boundaries[2].lng(),boundaries[0].lng(),6));
-		var markerOptions = {
-	    		position: mark[pointInt],
-	            map: map,
-	            title: 'Hello World! ' + pointInt
+		var mapProp = {
+				center: dasma,
+				zoom: 10,
+				mapTypeId: google.maps.MapTypeId.HYBRID
 			};
-		//generate random LatLng coordinate
-		if (google.maps.geometry.poly.containsLocation(mark[pointInt], polygon))
+		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+		var oms = new OverlappingMarkerSpiderfier(map);
+			
+		var mcOptions = {gridSize: 50, maxZoom: 15};
+		var markers = [];
+			
+		for (var pt_ctr = 0; pt_ctr < lat.length; pt_ctr++) 
 		{
-			marker[pointInt] = new google.maps.Marker(markerOptions);
+			var marker_icon = "";
+			if (type[pt_ctr] == "0")
+				marker_icon = img_icon[0];
+			else if (type[pt_ctr] == "1")
+				marker_icon = img_icon[1];
 			
-			infowindow[pointInt] = new google.maps.InfoWindow;
-			var infoContent = 'Latitude: ' + mark[pointInt].lat() + '<br/>Longitude: ' + mark[pointInt].lng();
-
-			bindInfoW(marker[pointInt], infoContent, infowindow[pointInt], map);
+			var marker = new google.maps.Marker({
+								position: new google.maps.LatLng(
+									lat[pt_ctr],
+									lng[pt_ctr]
+								),
+						map: map,
+						icon : marker_icon
+			});
 			
-			pointInt++;
+			var html = "test" + pt_ctr;
+				
+			marker.info = new google.maps.InfoWindow({
+						content: html
+					});
+			oms.addListener('click', function(marker) {
+				 marker.info.open(map, marker);
+					});
+			
+			oms.addListener('spiderfy', function(markers) {
+		        for(var i = 0; i < markers.length; i ++) {
+		          marker.setIcon(iconWithColor(spiderfiedColor));
+		          marker.setShadow(null);
+		        } 
+		        marker.info.close();
+		      });
+					
+			oms.addMarker(marker);
+			markers.push(marker);
 		}
-	}
+					
+		//var mc = new MarkerClusterer(map, markers, mcOptions);
 }
-
-//got from http://stackoverflow.com/questions/6878761/javascript-how-to-create-random-longitude-and-latitudes
-//another note http://stackoverflow.com/questions/8611830/javascript-random-positive-or-negative-number
-//another http://stackoverflow.com/questions/11400290/putting-random-latitude-and-longitude-in-google-maps-geolocation
-function getRandomInRange(from, to, fixed) {
-    return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
-    // .toFixed() returns string, so ' * 1' is a trick to convert to number
-}
-
-// got form http://stackoverflow.com/questions/4381355/multiple-google-maps-infowindow
-function bindInfoW(marker, contentString, infowindow, map)
-{
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(contentString);
-            infowindow.open(map, marker);
-        });
-}
-
 google.maps.event.addDomListener(window, 'load', initialize);
