5599bce89e877e823a3dd27fe010180e0936ebd2
darpell <pelonia.darius@gmail.com>
2014-02-27 11:11:13 +0800

    refactored the code in displaying cases but now with a problem with pagination

--- bootstrap/application/views/site/dashboard.php
+++ bootstrap/application/views/site/dashboard.php
@@ -46,10 +46,10 @@ body { height:100% }
 			</div>
 			<div class="panel-body">
 				<ul class="nav nav-pills nav-stacked">
-				  <li> <a href="<?= site_url('website/cases/view_suspected') ?>"> Suspected <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'suspected'))->num_rows(); ?></span> </a> </li>
-				  <li> <a href="<?= site_url('website/cases/view_threatening') ?>"> Threatening <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'threatening'))->num_rows(); ?></span> </a> </li>
-				  <li> <a href="<?= site_url('website/cases/view_serious') ?>"> Serious <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'serious'))->num_rows();?></span> </a> </li>
-				  <li> <a href="<?= site_url('website/cases/view_hospitalized') ?>"> Hospitalized <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'hospitalized'))->num_rows()?></span> </a> </li>
+				  <li> <a href="<?= site_url('website/cases/view/suspected') ?>"> Suspected <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'suspected'))->num_rows(); ?></span> </a> </li>
+				  <li> <a href="<?= site_url('website/cases/view/threatening') ?>"> Threatening <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'threatening'))->num_rows(); ?></span> </a> </li>
+				  <li> <a href="<?= site_url('website/cases/view/serious') ?>"> Serious <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'serious'))->num_rows();?></span> </a> </li>
+				  <li> <a href="<?= site_url('website/cases/view/hospitalized') ?>"> Hospitalized <span class="badge pull-right"><?php echo $this->db->get_where('active_cases',array('status' => 'hospitalized'))->num_rows()?></span> </a> </li>
 				</ul>
 			</div>
 		</div>
