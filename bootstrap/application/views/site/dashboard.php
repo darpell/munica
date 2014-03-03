4ba1a086c812c34646740a1b7cf2c50fb99cd7d4
darpell <pelonia.darius@gmail.com>
2014-03-03 13:59:05 +0800

    fixed panel heading of case map

--- bootstrap/application/views/site/dashboard.php
+++ bootstrap/application/views/site/dashboard.php
@@ -139,7 +139,7 @@ body { height:100% }
 	<!-- Map -->
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title"> Case Map </h3>
+				<h3 class="panel-title"> Case Map of current active cases</h3>
 			</div>
 			<div class="panel-body">
 				<div class="col-md-8">
