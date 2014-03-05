34a1c588e06b70aaeed84b438c819e1428042dc9
darpell <pelonia.darius@gmail.com>
2014-03-05 08:42:41 +0800

    fixed search

--- bootstrap/application/views/site/search_results.php
+++ bootstrap/application/views/site/search_results.php
@@ -22,12 +22,14 @@
 				<table class="table">
 					<thead>
 						<tr>
-							<th> Person Name </th> <th> Gender </th> <th> Fever Duration </th> <th> Suspected Source </th> <th> Contact Number </th>
+							<th> House No </th> <th> Household Name</th> <th> Person Name </th> <th> Gender </th> <th> Fever Duration </th> <th> Suspected Source </th> <th> Contact Number </th>
 						</tr>
 					<thead>
 					<tbody>
 						<?php foreach ($results as $case) {?>
 						<tr>
+							<td> <?= $case['house_no']?> </td>
+							<td> <?= $case['household_name']?> </td>
 							<td> <a href="<?= site_url('website/cases/view_person/' . $case['imcase_no']) ?>"><?= $case['person_first_name'] . ' ' . $case['person_last_name'] ?> </a> </td> 
 							<td> <?= $case['person_sex'] ?> </td>
 							<td> <?= $case['days_fever'] ?> </td> 
