6616f3b8ccb2fd47be9be2d8ab4c55a2f0b710b3
darpell <pelonia.darius@gmail.com>
2014-03-05 09:08:06 +0800

    fixed input value in notif

--- bootstrap/application/models/notif_model.php
+++ bootstrap/application/models/notif_model.php
@@ -52,7 +52,7 @@ class Notif_model extends CI_Model
 		{
 			$notif_input = array(
 					'notif_type'		=> 'update',
-					'notification'		=> 'Please update on' . $query[$ctr]['person_first_name'] . ' ' . $query[$ctr]['person_last_name'] . ' status',
+					'notification'		=> 'Please update on ' . $query[$ctr]['person_first_name'] . ' ' . $query[$ctr]['person_last_name'] . ' status',
 					'unique_id'			=> 'website/view_person/' . $query[$ctr]['imcase_no'],
 					'notif_viewed'		=> 'N',
 					'notif_createdOn'	=> date('Y-m-d H:i:s'),
