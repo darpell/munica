6100b9c66e70e1dc3d60663ec7c30eb828c4b3c4
darpell <pelonia.darius@gmail.com>
2014-03-06 14:41:26 +0800

    added tag that a person has fever or hospitalized

--- bootstrap/application/views/site/admin/person_filter.php
+++ bootstrap/application/views/site/admin/person_filter.php
@@ -14,8 +14,27 @@
 			<div class="panel-body">
 				<ul class="nav nav-pills nav-stacked">
 					<?php foreach ($persons as $person) { $per = $person['person_id']; $name = $person['person_first_name'] . ' ' . $person['person_last_name']; ?>
-					<li> <a href="<?=site_url('website/households/view_person/' . $per) ?>"> <?php echo $name; ?> </a> </li>
+					<?php $this->load->model('hh_model','model');
+						$active_case = $this->model->check_if_has_fever($person['person_id']);
+					if ($active_case != NULL)
+					{
+						if ($active_case['status'] == 'hospitalized') //is sick and hospitalized
+						{
+				?>
+					
+					<li> <a href="<?=site_url('website/households/view_person/' . $per) ?>"> <label style="color:GREEN;"> [Hospitalized] </label> <?php echo $name; ?> </a> </li>
+					<?php
+						}
+						else //is sick but unhospitalized
+						{
+							//$this->masterlist->add_fever_day($household_persons[$ctr]['person_id']);
+					?>
+						<li> <a href="<?=site_url('website/households/view_person/' . $per) ?>"> <label style="color:RED;"> [Has fever] </label> <?php echo $name; ?> </a> </li>
 					<?php } ?>
+					
+					<?php } else { ?>
+						<li> <a href="<?=site_url('website/households/view_person/' . $per) ?>"> <label style="color:GREEN;"> </label> <?php echo $name; ?> </a> </li>
+					<?php } } ?>
 				</ul>
 				<?php echo $links; ?>
 			</div>
